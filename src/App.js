5dde0a772995caa9edfe191844aa90813c74bc58
RussellShire <russell.shire@gmail.com>
2022-09-27 15:10:35 +0100

    Add the ability to create new counters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Counter from './Counter/Counter';
+import { React, useState } from 'react';
 
 function App() {
+  const [counters, setCounters] = useState([<Counter /> ])
+  
+  // const addCounter = () => setCounters(...counters, counters.push(<Counter />))
+
+  const addCounter = () => {
+    let moreCounters = [...counters, <Counter />]
+    setCounters(moreCounters)
+
+}
+  
   return (
     <div className="App">
-      <Counter />
-      <Counter />
+      <button onClick={addCounter}>Add counter</button>
+      {counters}
     </div>
   );
 }
